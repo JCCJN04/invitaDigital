ea35f26996ab73873dde62ffa9f9b793b3d2ca54
juancmc71 <mendoza.castillo04@gmail.com>
2026-03-01 15:48:22 -0600

    feat: Add rewrite rule for /paulaxv/_next paths in Vercel and Next.js configurations.

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
       { source: "/citliyamed", destination: "https://citliyamed.vercel.app/" },
       { source: "/citliyamed/:path*", destination: "https://citliyamed.vercel.app/:path*" },
       { source: "/paulaxv", destination: "https://paula-sage.vercel.app/" },
+      { source: "/paulaxv/_next/:path*", destination: "https://paula-sage.vercel.app/_next/:path*" },
       { source: "/paulaxv/:path*", destination: "https://paula-sage.vercel.app/:path*" },
     ];
   },
